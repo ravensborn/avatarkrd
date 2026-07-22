95471d99c02d18d4081782cb085fc693ceb8c6ad
Arivan Ismail <187494087+arivanismail-05@users.noreply.github.com>
2026-07-22 13:48:21 +0300

    refactor: remove unused user retrieval route from API

--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-
 Route::post('/register' , [AuthController::class, 'register']);
