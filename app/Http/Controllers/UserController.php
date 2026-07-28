346ae26d27a1c09d9191e0b4641497c1244b410c
FarmanOthman <farmaanothmaan@gmail.com>
2026-07-28 15:24:52 +0300

    feat: add get user endpoint to retrieve user details

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function show(User $user)
+    {
+        return $user;
+    }
+}
